901173158a95dba369183b4dc6b31c51bc4bd415
SergiiBartytskyi <108729124+SergiiBartytskyi@users.noreply.github.com>
2024-03-09 23:41:20 +0100

    update_portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,18 @@
 const listEl = document.querySelector(".js-filter");
 
-const examples = document.querySelectorAll(".js-project__item");
-listEl.addEventListener("click", onProjectClick);
+// const examples = document.querySelectorAll(".js-project__item");
+// listEl.addEventListener("click", onProjectClick);
 
-function onProjectClick(e) {
-	examples.forEach(element => {
-		if (element.dataset.option === "all") {
-			element.style.display = "block";
-		} else if (e.target.dataset.option !== element.dataset.option && e.target.dataset.option !== "all") {
-			element.style.display = "none";
-		} else {
-			element.style.display = "block";
-		}
-	});
-}
+// function onProjectClick(e) {
+// 	examples.forEach(element => {
+// 		if (element.dataset.option === "all") {
+// 			element.style.display = "block";
+// 		} else if (e.target.dataset.option !== element.dataset.option && e.target.dataset.option !== "all") {
+// 			element.style.display = "none";
+// 		} else {
+// 			element.style.display = "block";
+// 		}
+// 	});
+// }
+
+
